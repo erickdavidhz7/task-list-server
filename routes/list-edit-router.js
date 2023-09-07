606fa27040f7001d422b7324c5868f903d22ab87
Erick Henriquez <erickdavid7@hotmail.com>
2023-09-07 12:11:05 -0500

    se ha implementado la ruta de edición

--- routes/list-edit-router.js
+++ routes/list-edit-router.js
@@ -2,18 +2,29 @@ const express = require("express");
 const listEditRouter = express.Router();
 const taskList = require("../taskList.json");
 
-listViewRouter
+listEditRouter
 .route("/list_edit")
-.get((req, res) => {
-  let newTaskList = taskList.filter(task => task.status !== "Not completed");
-  res.status(200).send({taskListCompleted : newTaskList})
+.post((req, res) => {
+  const newTask = req.body;
+  taskList.push(newTask);
+  res.send({taskList_edited : taskList});
 })
 
-listViewRouter
-.route("/list_edit")
-.get((req, res) => {
-  let newTaskList = taskList.filter(task => task.status === "Not completed");
-  res.status(200).send({taskListNotCompleted : newTaskList})
+listEditRouter
+.route("/list_edit/:id")
+.put((req, res) => {
+  const id = req.params.id;
+  const taskUpdate = req.body;
+  console.log(taskUpdate);
+  const newTaskList = taskList.map(task => {
+    if(task.id == id) return { ...task, name: taskUpdate.name, description: taskUpdate.description, status: taskUpdate.status};
+    else return task;
+  })
+  res.send({taskList_edited: newTaskList})
 })
-
+.delete((req, res) => {
+  const id = req.params.id;
+  const newTaskList = taskList.filter(task => task.id != id);
+  res.send({taskList_edited : newTaskList});
+})
 module.exports = listEditRouter;
--- serverNode.js
+++ serverNode.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const port = 8000;
 const listViewRouter = require("./routes/list-view-router");
+const listEditRouter = require("./routes/list-edit-router");
 const taskList = require("./taskList.json");
+
 app.use(express.json());
 app.use(listViewRouter);
+app.use(listEditRouter);
 
 app.get("/", (req, res) => {
   res.status(200).send("Welcome!");
