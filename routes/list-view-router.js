59a947986430f6425fa5788c72e8bd16e374f9c7
Erick Henriquez <erickdavid7@hotmail.com>
2023-09-24 19:53:35 -0500

    se implementó redireccionamiento con list_view

--- routes/list-view-router.js
+++ routes/list-view-router.js
@@ -2,18 +2,29 @@ const express = require("express");
 const listViewRouter = express.Router();
 const taskList = require("../taskList.json");
 
-listViewRouter
-.route("/list_view_completed")
-.get((req, res) => {
-  let newTaskList = taskList.filter(task => task.status !== "Not completed");
-  res.status(200).send({taskListCompleted : newTaskList})
-})
+listViewRouter.route("/list_view").get((req, res) => {
+  res.status(200).send({ taskList: taskList });
+});
 
-listViewRouter
-.route("/list_view_not_completed")
-.get((req, res) => {
+listViewRouter.route("/list_view/:type").get((req, res) => {
+  const type = req.params.type;
+  if (type === "completed") {
+    res.redirect("/list_view_completed");
+  } else if (type === "not_completed") {
+    res.redirect("/list_view_not_completed");
+  } else {
+    res.status(400).send({ error: "Invalid path name!" });
+  }
+});
+
+listViewRouter.route("/list_view_completed").get((req, res) => {
+  let newTaskList = taskList.filter(task => task.status !== "Not completed");
+  res.status(200).send({ taskListCompleted: newTaskList });
+});
+
+listViewRouter.route("/list_view_not_completed").get((req, res) => {
   let newTaskList = taskList.filter(task => task.status === "Not completed");
-  res.status(200).send({taskListNotCompleted : newTaskList})
-})
+  res.status(200).send({ taskListNotCompleted: newTaskList });
+});
 
 module.exports = listViewRouter;
--- serverNode.js
+++ serverNode.js
@@ -3,15 +3,14 @@ const app = express();
 const port = 8000;
 const listViewRouter = require("./routes/list-view-router");
 const listEditRouter = require("./routes/list-edit-router");
-const taskList = require("./taskList.json");
 
 app.use(express.json());
 app.use(listViewRouter);
 app.use(listEditRouter);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin" , "*");
-  res.header("Access-Control-Allow-Methods", ["GET", "POST", "PUT", "DELETE"])
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", ["GET", "POST", "PUT", "DELETE"]);
   next();
 });
 
@@ -19,12 +18,8 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome!");
 });
 
-app.get("/this-should-exists", (req, res)=>{
-  res.status(404).send({error: "Not found"});
-});
-
-app.get("/list_view", (req, res) => {
-  res.status(200).send({ taskList: taskList });
+app.get("/this-should-exists", (req, res) => {
+  res.status(404).send({ error: "Not found" });
 });
 
 app.listen(port, () => {
